f3bed709ee5a9f31a8fbbaede5820828053e581b
ejixxer999 <lehman.ethan2@gmail.com>
2022-02-03 21:34:21 -0500

    added client side routes using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { Route, Switch } from "react-router-dom"
+import NavBar from "./NavBar";
+import About from "./About";
+import Contact from "./Contact"
+import Home from "./Home"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Fresh Look Details!</h1>
+      <NavBar />
+        <Switch>
+            <Route exact path="/about">
+                <About />
+            </Route>
+
+            <Route exact path="/contact">
+                <Contact />
+            </Route>
+
+            <Route exact path="/">
+                <Home />
+            </Route>
+        </Switch>
     </div>
   );
 }
