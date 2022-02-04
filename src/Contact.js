973a9b1be1e93217718daf287b695c60784461de
ejixxer999 <lehman.ethan2@gmail.com>
2022-02-03 21:34:37 -0500

    created a contact page

---
+++ src/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Contact() {
+
+    return(
+        <div>
+            <h1>you have made it to the contact page</h1>
+        </div>
+    )
+}
+
+export default Contact
