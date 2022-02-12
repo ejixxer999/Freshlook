9465a956382afa1e818d2e8ca74a1e4e9159d161
ejixxer999 <lehman.ethan2@gmail.com>
2022-02-12 14:56:13 -0500

    added home page styling

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,10 +4,13 @@ function Home(){
 
 
     return(
-        <div>
+        <div id="home">
+            <h1 style={{ color: "white", width: "300px", fontSize: "35px"}}>Fresh Look Details</h1>
+            <h3 style={{ color: "white"}}>Welcome To The World of Luxury Detailing</h3>
 
         </div>
     )
 }
 
 export default Home
+
